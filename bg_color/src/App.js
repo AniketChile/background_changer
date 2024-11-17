94f1ce50b0e2fdf50677844f4f3e64ed83e00fe0
AniketChile <84616134+AniketChile@users.noreply.github.com>
2024-11-17 11:16:29 +0000

    CurrencyConverter

---
+++ bg_color/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { useCallback, useState } from 'react';
+
+function App() {
+ 
+  return (
+    <div className='bg-black'>
+          <h1 className='bg-green-500 p-4 rounded-full italic font-semibold text-black  text-4xl text-center'>Currency Converter</h1>
+
+    </div>
+    
+  );
+}
+
+export default App;
